2b4aab2c9df6f8508f9121c3f7fd30cfc2662d01
Mykhailo Stadnyk <mikhus@gmail.com>
2018-10-08 14:26:23 +0300

    user service base implementation done

--- src/User.ts
+++ src/User.ts
@@ -20,20 +20,141 @@ import {
     IMQService,
     expose,
     profile,
+    property,
+    IMessageQueue,
 } from '@imqueue/rpc';
+import * as mongoose from 'mongoose';
 
+/**
+ * Serializable user type
+ */
+export class UserObject {
+    @property('string', true)
+    id?: string;
+
+    @property('string')
+    email: string;
+
+    @property('string')
+    password: string;
+
+    @property('boolean')
+    isActive: boolean;
+
+    @property('string')
+    firstName: string;
+
+    @property('string')
+    lastName: string;
+}
+
+/**
+ * User service implementation
+ */
 export class User extends IMQService {
-    // Implement your service methods here, example:
+
+    private db: mongoose.Connection;
+    private UserModel: mongoose.Model<any>;
+
     /**
-     * Returns "Hello!" string.
-     * This method is just an example of implementation. Please, remove it
-     * and write your service methods instead.
-     * 
-     * @return {string}
+     * Initializes mongo database connection and user schema
+     *
+     * @return Promise<any>
+     */
+    private async initDb(): Promise<any> {
+        return new Promise((resolve, reject) => {
+            mongoose.connect(
+                'mongodb://localhost/user',
+                { useNewUrlParser: true },
+            );
+            this.db = mongoose.connection;
+            this.db.on('error', reject);
+            this.db.once('open', resolve);
+            const schema = new mongoose.Schema({
+                id: mongoose.SchemaTypes.ObjectId,
+                email: mongoose.SchemaTypes.String,
+                password: mongoose.SchemaTypes.String,
+                isActive: mongoose.SchemaTypes.Boolean,
+                firstName: mongoose.SchemaTypes.String,
+                lastName: mongoose.SchemaTypes.String,
+            });
+            this.UserModel = mongoose.model('User', schema);
+        });
+    }
+
+    /**
+     * Overriding start method to inject mongodb connection establishment
+     */
+    public async start(): Promise<IMessageQueue | undefined> {
+        this.logger.log('Initializing MongoDB connection...');
+        await this.initDb();
+        return super.start();
+    }
+
+    /**
+     * Creates a new user instance in the system
+     *
+     * @param {UserObject} data - user data fields
+     * @return {Promise<UserObject>} - saved user data object
      */
     @profile()
     @expose()
-    public hello(): string {
-        return "Hello!";
+    public async create(data: UserObject): Promise<UserObject> {
+        const user = new this.UserModel(data);
+        await user.save();
+        return user as UserObject;
+    }
+
+    /**
+     * Updates existing user with the new data set
+     *
+     * @param {string} id - user identifier in the system
+     * @param {UserObject} data - user data fields
+     * @return {Promise<UserObject>} - saved user data object
+     */
+    @profile()
+    @expose()
+    public async update(id: string, data: UserObject): Promise<UserObject> {
+        const user = await this.UserModel.findByIdAndUpdate(id, data).exec();
+        return user as UserObject;
+    }
+
+    /**
+     * Activates user in the system
+     *
+     * @param {string} id - user identifier in the system
+     * @return {Promise<boolean>} - operation execution result
+     */
+    @profile()
+    @expose()
+    public async activate(id: string): Promise<boolean> {
+        try {
+            await this.UserModel
+                .findByIdAndUpdate(id, { isActive: true })
+                .exec();
+            return true;
+        } catch (err) {
+            this.logger.warn('Error when activating user:', err);
+            return false;
+        }
+    }
+
+    /**
+     *
+     * @param {string} id - user identifier in the system
+     * @return {Promise<boolean>} - operation execution result
+     */
+    @profile()
+    @expose()
+    public async deactivate(id: string): Promise<boolean> {
+        try {
+            await this.UserModel
+                .findByIdAndUpdate(id, { isActive: false })
+                .exec();
+            return true;
+        } catch (err) {
+            this.logger.warn('Error when deactivating user:', err);
+            return false;
+        }
     }
 }
